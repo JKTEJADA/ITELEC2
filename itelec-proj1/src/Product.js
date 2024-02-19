fae1e244478dfadc89a1801ef0f55bedaf996462
JOSEPH KIEL TEJADA <144079651+JKTEJADA@users.noreply.github.com>
2024-02-19 07:50:24 +0000

    new

--- itelec-proj1/src/Product.js
+++ itelec-proj1/src/Product.js
@@ -1,27 +1,28 @@
+function Product(props) {
+  let isSoldout = props.soldout ? 1 : 0;
 
-
-function Product(props){
-    return( 
-      <div className='Products'>
-        <div class="card">
-        <img src= {props.photoname} class="card-img-top" alt="..."/>
+  return (
+    <div className="Products" style={{ filter: `grayscale(${isSoldout})` }}>
+      <div class="card">
+        <img src={props.photoname} class="card-img-top" alt="..." />
         <div class="card-body">
-          <div className='d-flex align-items-center'>
-            <p className='card-text text-start m-0 flex-fill'>{props.name}</p>
+          <div className="d-flex align-items-center">
+            <p className="card-text text-start m-0 flex-fill">
+              {props.name} {props.soldout ? "[Sold Out]" : ""}
+            </p>
             <span>
-              <img src='/icons/heart.svg' width = '15' className='mx-2'/>
+              <img src="/icons/heart.svg" width="15" className="mx-2" />
               {props.likes}
             </span>
             <span>
-              <img src='/icons/eye.svg' width = '15' className='mx-2'/>
+              <img src="/icons/eye.svg" width="15" className="mx-2" />
               {props.views}
             </span>
-  
           </div>
         </div>
       </div>
-      </div>
-    );
-  }
+    </div>
+  );
+}
 
-  export default Product;
+export default Product;
